5be9354ecf7b0e63393b32583971436ff1128b92
Ümit Tunç <umitreva@gmail.com>
2025-11-07 23:29:35 +0300

    Add Turkish Localization for User Resource: Created a new language file for user-related labels and messages in Turkish, ensuring proper localization support for user management features.

---
+++ lang/vendor/filament-users/tr/user.php
@@ -0,0 +1,67 @@
+<?php
+
+return [
+    'group' => '',
+    'resource' => [
+        'id' => 'ID',
+        'single' => 'Kullanıcı',
+        'email_verified_at' => 'E-posta Doğrulama',
+        'created_at' => 'Oluşturulma',
+        'updated_at' => 'Güncellenme',
+        'verified' => 'Doğrulandı',
+        'unverified' => 'Doğrulanmadı',
+        'name' => 'Ad',
+        'email' => 'E-posta',
+        'password' => 'Şifre',
+        'password_confirmation' => 'Şifre Onayı',
+        'roles' => 'Roller',
+        'teams' => 'Takımlar',
+        'label' => 'Kullanıcılar',
+        'title' => [
+            'show' => 'Kullanıcıyı Görüntüle',
+            'delete' => 'Kullanıcıyı Sil',
+            'impersonate' => 'Kullanıcıyı Taklit Et',
+            'create' => 'Kullanıcı Oluştur',
+            'edit' => 'Kullanıcıyı Düzenle',
+            'list' => 'Kullanıcılar',
+            'home' => 'Kullanıcılar',
+        ],
+        'notificaitons' => [
+            'last' => [
+                'title' => 'Hata',
+                'body' => 'Son kullanıcıyı silemezsiniz',
+            ],
+            'self' => [
+                'title' => 'Hata',
+                'body' => 'Kendinizi silemezsiniz',
+            ],
+        ],
+        'avatar' => 'Avatar',
+        'change_password' => 'Şifreyi Değiştir',
+        'change_password_auto' => 'Şifre otomatik olarak değiştirildi',
+        'change_password_success' => 'Şifre başarıyla değiştirildi',
+        'change_password_auto_body' => 'Şifre otomatik olarak değiştirildi',
+        'change_password_success_body' => 'Şifre başarıyla değiştirildi',
+        'change_password_auto_body_placeholder' => 'Otomatik oluşturmak için boş bırakın',
+        'change_password_success_body_placeholder' => 'Otomatik oluşturmak için boş bırakın',
+    ],
+    'bulk' => [
+        'teams' => 'Takımları Güncelle',
+        'roles' => 'Rolleri Güncelle',
+    ],
+    'team' => [
+        'title' => 'Takımlar',
+        'single' => 'Takım',
+        'columns' => [
+            'avatar' => 'Avatar',
+            'name' => 'Ad',
+            'owner' => 'Sahibi',
+            'personal_team' => 'Kişisel Takım',
+        ],
+    ],
+    'banner' => [
+        'impersonating' => 'Taklit modu',
+        'leave' => 'Taklitten Çık',
+    ],
+];
+
